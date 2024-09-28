bdbb6eaea5d24fecd51f8600f4cdf39d3f5b25f5
wpiyaphon <wupiyaphon@gmail.com>
2024-09-28 22:41:13 +0700

    updated: screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
   fontFamily: {
     sans: ["LineSeed"],
   },
